34e8464b2581c8ca3530fed2223d020ee22dfa3c
mengfanhua <1070985939@qq.com>
2020-03-29 15:02:20 +0800

    学生助手app后台控制模块

---
+++ student_helper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentHelperConfig(AppConfig):
+    name = 'student_helper'
---
+++ student_helper/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+
+@csrf_exempt
+def get_class(request):
+    # if request.method == "POST":
+    list1 = [{"a": "1", "b": "2"}]
+    return HttpResponse(json.dumps(list1, ensure_ascii=False))
+
+# use this module to build api for software
