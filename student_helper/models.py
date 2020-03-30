f7ce1641b3ee7013d2f647284c16a46c9076a82b
mengfanhua <1070985939@qq.com>
2020-03-30 19:55:23 +0800

    在django中创建了数据库，可使用orm方式调用

--- student_helper/models.py
+++ student_helper/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    id = models.CharField(max_length=8, primary_key=True)
+    password = models.CharField(max_length=20)
+
+
+class ClassPlan(models.Model):
+    id = models.CharField(max_length=8, primary_key=True)
+    cid = models.CharField(max_length=20)
+    cname = models.CharField(max_length=70)
+    pid = models.CharField(max_length=4, null=True, blank=True)
+    pname = models.CharField(max_length=30, null=True, blank=True)
+    week = models.CharField(max_length=54)
+    position = models.CharField(max_length=23)
+
+    class Meta:
+        unique_together = ("id", "cid", "week", "position")
+
+
+class poem(models.Model):
+    id = models.CharField(max_length=6, primary_key=True)
+    sentence = models.CharField(max_length=50)
+    origin = models.CharField(max_length=40)
