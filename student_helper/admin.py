b7196a0690259ebbda7ae1e91955a656f7cb253d
mengfanhua <1070985939@qq.com>
2020-04-16 11:14:24 +0800

    添加了管理员权限，并且注册了数据库表

--- student_helper/admin.py
+++ student_helper/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+admin.site.register(models.Users)  # models 中你要注册的表 不注册不会显示
+admin.site.register(models.ClassPlan)
